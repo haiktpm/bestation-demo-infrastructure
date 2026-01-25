f9fa5ed6da23a3cc1d4c2191bfccbe9df2ccc7a6
haiktpm <haitv2809@gmail.com>
2026-01-25 23:18:21 +0700

    Fix provider issue

--- network/versions.tf
+++ network/versions.tf
@@ -2,10 +2,6 @@ terraform {
   required_version = ">= 1.0.0"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.63"
-    }
     tls = {
       source  = "hashicorp/tls"
       version = ">= 3.1.0"
