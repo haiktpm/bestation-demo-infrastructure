d9fa54943597bb73b4d05843fc2f7eeb750635cf
haiktpm <haitv2809@gmail.com>
2026-01-25 22:19:12 +0700

    feat: Configure AWS provider and Terraform Cloud workspaces for EKS and network infrastructure.

--- eks/providers.tf
+++ eks/providers.tf
@@ -1,3 +1,20 @@
 provider "aws" {
   region = "ap-southeast-1"
 }
+
+terraform {
+  cloud {
+    organization = "haiktpm"
+
+    workspaces {
+      name = "bestation-demo-infrastructure"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
--- network/providers.tf
+++ network/providers.tf
@@ -1,3 +1,20 @@
 provider "aws" {
   region = "ap-southeast-1"
 }
+
+terraform {
+  cloud {
+    organization = "haiktpm"
+
+    workspaces {
+      name = "bestation-demo-network"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
