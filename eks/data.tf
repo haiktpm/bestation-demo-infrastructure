077d7edbe870e1e08aaa2f22b106b15622bb131e
haiktpm <haitv2809@gmail.com>
2026-01-28 21:02:51 +0700

    Fix eks  data block

--- eks/data.tf
+++ eks/data.tf
@@ -4,14 +4,20 @@ data "aws_vpcs" "vpcs" {
     cost    = "bestarion-demo"
     }
 }
-data "aws_subnet_ids" "all_subnet" {
-  vpc_id    = tolist(data.aws_vpcs.vpcs.ids)[0]
+data "aws_subnets" "all_subnet" {
+  filter {
+    name   = "vpc-id"
+    values = tolist(data.aws_vpcs.vpcs.ids)[0]
+  }
   tags      = {
     App     = "k8s"
   }
 }
-data "aws_subnet_ids" "private" {
-  vpc_id    = tolist(data.aws_vpcs.vpcs.ids)[0]
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = tolist(data.aws_vpcs.vpcs.ids)[0]
+  }
   tags      = {
     Type    = "Private"
   }
